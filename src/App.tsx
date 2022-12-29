bc669ee4471c093232c6728cc09883290dc47f44
renato-17 <renato.avela.17@gmail.com>
2022-12-29 18:17:09 -0500

    <config>: Base project configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react'
+
+function App (): ReactElement {
+  return (
+    <h1 className='text-4xl font-bold underline bg-black text-white'>
+      Hello world
+    </h1>
+  )
+}
+
+export default App
