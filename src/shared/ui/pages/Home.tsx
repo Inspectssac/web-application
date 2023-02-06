aa3075aafa1b433c40d9dea6c44f7690ac809c41
renato-av <renato.avela.17@gmail.com>
2023-02-06 10:18:27 -0500

    <fix>: fixing some design in dashboard

--- src/shared/ui/pages/Home.tsx
+++ src/shared/ui/pages/Home.tsx
@@ -4,23 +4,24 @@ import useMediaQuery from '@/shared/hooks/userMediaQuery'
 
 const Home = (): ReactElement => {
   const isAboveSmallScreens = useMediaQuery('(min-width: 640px)')
-  const [routesCount, setRoutesCount] = useState(0)
+  // const [routesCount, setRoutesCount] = useState(0)
 
-  useEffect(() => {
-    const routeServices = new RouteServices()
-    void routeServices.getAll()
-      .then(response => {
-        setRoutesCount(response.length)
-      })
-  }, [])
+  // useEffect(() => {
+  //   const routeServices = new RouteServices()
+  //   void routeServices.getAll()
+  //     .then(response => {
+  //       setRoutesCount(response.length)
+  //     })
+  // }, [])
 
   return (
     <div className='container'>
       <main className={`${isAboveSmallScreens ? 'grid grid-cols-4 gap-2' : ''}`}>
-        <div className='shadow-md hover:shadow-xl bg-white rounded-md py-2 px-4 text-center'>
+        <h1>INSPECTESSAC</h1>
+        {/* <div className='shadow-md hover:shadow-xl bg-white rounded-md py-2 px-4 text-center'>
           <h1 className='text-xl uppercase'>Active routes</h1>
           <p className='text-lg font-bold'>{routesCount}</p>
-        </div>
+        </div> */}
       </main>
     </div>
   )
